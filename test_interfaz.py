5bd58e525a6faaf77db2a727a83d021e8b329ddb
Juan Manuel <jmromben@Juans-MacBook-Pro.local>
2026-04-09 21:09:19 +0200

    TESTS

--- test_interfaz.py
+++ test_interfaz.py
@@ -13,7 +13,13 @@ def test_pedir_modo_colocacion_valido(monkeypatch):
 def test_pedir_modo_colocacion_invalido_luego_valido(monkeypatch, capsys):
     # Simulamos que falla la primera vez ("3") y acierta la segunda ("2")
     entradas = ["3", "2"]
-    monkeypatch.setattr('builtins.input', lambda _: entradas.pop(0))
+    
+    # Creamos un mock de input que SÍ imprima el texto en la consola para que capsys lo lea
+    def mock_input(texto_del_input):
+        print(texto_del_input) 
+        return entradas.pop(0) 
+        
+    monkeypatch.setattr('builtins.input', mock_input)
     resultado = interfaz.pedir_modo_colocacion("Jugador 2")
     
     salida = capsys.readouterr().out
